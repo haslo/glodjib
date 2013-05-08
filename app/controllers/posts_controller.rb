e3ff430ac75952d8b717e5d660f787569db71eb5
Guido Gloor Modjib @Home <haslo@haslo.ch>
2013-05-08 13:42:53 +0200

    Changed translations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create!(params[:post])
-    flash[:notice] = "New post created."
+    flash[:notice] = I18n.t('notices.post.created')
     redirect_to posts_path
   end
 end
