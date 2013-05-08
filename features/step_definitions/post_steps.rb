d460473277024b876a1ca62c5a3730ad694d0466
Guido Gloor Modjib @Home <haslo@haslo.ch>
2013-05-08 12:14:51 +0200

    Step definitions for Cucumber

--- features/step_definitions/post_steps.rb
+++ features/step_definitions/post_steps.rb
@@ -8,6 +8,30 @@ Given /^I have no posts$/ do
   Post.delete_all
 end
 
+Given(/^I am on the list of posts$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I go to the list of posts$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I follow "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I fill in "(.*?)" with "(.*?)"$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I press "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
 Then /^I should have ([0-9]+) posts?$/ do |count|
   Post.count.should == count.to_i
 end
