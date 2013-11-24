ee6d661c8e2dfa5da4c550756190469b02610a13
Guido Gloor <haslo@haslo.ch>
2013-11-24 23:08:47 +0100

    Devise config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable,
-         :registerable,
-         :recoverable,
          :rememberable,
-         :trackable,
-         :validatable
+         :trackable
 end
