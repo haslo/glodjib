0f5601cd2ebfaa7110b1aaa9b6e5def29f8d7d37
Guido Gloor <haslo@haslo.ch>
2013-12-20 19:24:14 +0100

    Bugfix for tag display

--- app/controllers/post_tags_controller.rb
+++ app/controllers/post_tags_controller.rb
@@ -3,7 +3,8 @@ class PostTagsController < ApplicationController
   before_filter :authenticate_user!, :except => [:show]
 
   expose(:post_tag) { PostTag.where((params[:id].is_i? ? :id : :tag_text) => params[:id]).first }
-  expose(:posts) { post_tag.posts.sorted }
+  expose(:posts) { post_tag.posts.sorted.paginate(:page => params[:page], :per_page => Setting.posts_per_page || DEFAULT_POSTS_PER_PAGE) }
+  expose(:portfolios) { Hash[posts.map{|post| [post.id, Blog::PostService.portfolio(post.content)]}] }
 
   def show
     @title_parameter = post_tag.tag_text
