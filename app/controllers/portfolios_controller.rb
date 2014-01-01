ae07f63428993bf53090e780bacd9cf3a1cb59ed
Guido Gloor <haslo@haslo.ch>
2014-01-01 22:13:40 +0100

    Minor adjustments for galleries

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,7 +4,7 @@ class PortfoliosController < ApplicationController
 
   expose(:portfolio) { params[:portfolio_id] || params[:id] || 'portfolio' }
   expose(:gallery) { Gallery.get_with_id_or_shorthand(portfolio) }
-  expose(:images) { gallery.images }
+  expose(:images) { gallery.gallery_images.sorted.map(&:image) }
 
   def show
     @title_parameter = [I18n.t('titles.portfolios.portfolio'), portfolio.humanize].uniq.join(': ')
