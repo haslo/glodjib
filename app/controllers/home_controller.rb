d061570953bdfb1ed96ac7b881b7af76acb726ef
Guido Gloor <haslo@haslo.ch>
2014-01-01 11:35:17 +0100

    Special usage field added to galleries, updated from Settings

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,11 @@ class HomeController < ApplicationController
   layout 'minimal'
 
   expose(:background_images) do
-    # TODO revamp for Galleries
-    flickr_cache = Flickr::CacheService.find_or_create_cache(Setting.flickr_front_page_tag)
-    flickr_cache.flickr_tag.flickr_images.where(:flickr_user_id => flickr_cache.flickr_user.id)
+    images = []
+    Gallery.with_special_usage('frontpage').each do |frontpage_gallery|
+      images += frontpage_gallery.images
+    end
+    images
   end
   expose(:background_image) { background_images.sample }
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131231150054) do
+ActiveRecord::Schema.define(version: 20140101100021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20131231150054) do
     t.datetime "updated_at"
     t.boolean  "custom_shorthand", default: false
     t.integer  "position"
+    t.string   "special_usage"
   end
 
   create_table "galleries_images", force: true do |t|
